15cc66c130c1858affe44e77c3b959f53a504d0f
Surya A <surya.a@microfocus.com>
2022-05-08 10:29:18 +0530

    adding signature exmaple

--- src/main/java/signature/JavaSignature.java
+++ src/main/java/signature/JavaSignature.java
@@ -1,2 +1,46 @@
-package signature;public class JavaSignature {
+package signature;
+
+import java.nio.charset.StandardCharsets;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.SecureRandom;
+import java.security.Signature;
+import java.util.Base64;
+
+public class JavaSignature {
+    private static KeyPair keyPair;
+    private static SecureRandom secureRandom;
+
+    public static void generateKeyPair() throws Exception {
+        secureRandom = new SecureRandom();
+        KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("DSA");
+        keyPair = keyPairGenerator.generateKeyPair();
+    }
+
+    public byte[] generateSignature(String plainText) throws Exception {
+        Signature signature = Signature.getInstance("SHA256WithDSA");
+        signature.initSign(keyPair.getPrivate(), secureRandom);
+        signature.update(plainText.getBytes(StandardCharsets.UTF_8));
+        byte[] sign = signature.sign();
+        return sign;
+    }
+
+    public boolean verifySignature(String plainText, byte[] sign) throws Exception {
+        Signature signature = Signature.getInstance("SHA256WithDSA");
+        signature.initVerify(keyPair.getPublic());
+        signature.update(plainText.getBytes(StandardCharsets.UTF_8));
+        return signature.verify(sign);
+    }
+
+    public static void main(String[] args) throws Exception {
+        JavaSignature javaSignature = new JavaSignature();
+        JavaSignature.generateKeyPair();
+
+        String plaintext = "sdfghjklertyuiopcvbnm,";
+
+        byte[] sign = javaSignature.generateSignature(plaintext);
+        System.out.println("The signature is "  + Base64.getEncoder().encodeToString(sign));
+
+        System.out.println("Verify Signature: "  + javaSignature.verifySignature(plaintext, sign));
+    }
 }
